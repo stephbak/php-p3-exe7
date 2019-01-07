f9484a18963482377dc618f6b628458d208169bc
stephbak <stephane.bakum@sfr.fr>
2019-01-07 10:22:35 +0100

    p3 exe7

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<link rel="stylesheet" href="style.css" />
+<head>
+  <meta charset="utf-8" />
+  <title>php partie3</title>
+</head>
+<body>
+  <?php for($number = 1; $number <= 100; $number += 15){ ?>
+    <p>On tient le bon bout!</p>
+  <?php } ?>
+</body>
+</html>
